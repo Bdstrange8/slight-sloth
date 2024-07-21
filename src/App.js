243f105ab98c37302ac615bdb20b6ae75ba62e49
Benjamin Strange <benjaminstrange@my.unt.edu>
2024-07-20 21:12:54 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
 
+function Header() {
+  return (
+    <header>
+      <nav className='Nav-bar'>
+        <img alt="" src={logo} width="7%"/>
+        <ul className='Nav-items'>
+          <li> Main </li>
+          <li> About </li>
+          <li> Contact </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+function Body() {
+  return (
+  <div className='body'>
+  test text goes here
+  </div>
+  );
+}
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Body />
     </div>
   );
 }
